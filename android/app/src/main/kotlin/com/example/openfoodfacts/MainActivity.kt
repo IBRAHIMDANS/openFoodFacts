a5b159d241f2d91a6dea888a9459bdf4ccb1f0a4
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2021-03-23 16:56:10 +0100

    open food facts flutter

---
+++ android/app/src/main/kotlin/com/example/openfoodfacts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.openfoodfacts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:openfoodfacts/AppColors.dart';
+import 'package:openfoodfacts/res/app_vectorial_images.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: AppColors.blue,
+          primaryColorDark: AppColors.blueDark,
+          accentColor: AppColors.yellow,
+          appBarTheme: AppBarTheme(
+              elevation: 0.0,
+              backgroundColor: AppColors.white,
+              titleTextStyle: TextStyle(color: AppColors.blue),
+              actionsIconTheme: IconThemeData(color: AppColors.blue)),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              selectedItemColor: AppColors.blue,
+              unselectedItemColor: AppColors.gray2)),
+      home: EmptyListScreen(),
+    );
+  }
+}
+
+class EmptyListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mes scans',
+          style: TextStyle(color: Theme.of(context).primaryColor),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.bar_chart),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(AppVectorialImages.appLogo,
+                width: 300, height: 100),
+            SizedBox(height: screenHeight * 0.1),
+            FractionallySizedBox(
+              widthFactor: 0.3,
+              child: Text(
+                'Vous n\'avez pas encore scanné de produit',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.blue,
+                  height: 1.7,
+                  fontSize: 17.0,
+                ),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.1),
+            TextButton(
+              onPressed: () {
+                print('Clic');
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Commencer'.toUpperCase(),
+                  ),
+                  const SizedBox(width: 20.0),
+                  Icon(Icons.arrow_right_alt),
+                ],
+              ),
+              style: OutlinedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  vertical: 18.0,
+                  horizontal: 25.0,
+                ),
+                primary: AppColors.blue,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(22.0),
+                  ),
+                ),
+                backgroundColor: AppColors.yellow,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
